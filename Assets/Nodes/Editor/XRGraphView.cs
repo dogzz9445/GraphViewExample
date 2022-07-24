9069edef073c1766c0f719d9b3ee3b7856a19455
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2022-07-24 12:11:48 +0900

    Initialize project

---
+++ Assets/Nodes/Editor/XRGraphView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphProcessor;
+using UnityEditor;
+
+public class XRGraphView : BaseGraphView
+{
+    public XRGraphView(EditorWindow window) : base(window) { }
+}
---
+++ Assets/Nodes/Editor/XRGraphWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+
+namespace XRNodes.Editor
+{
+    public class XRGraphWindow : BaseGraphWindow
+    {
+        BaseGraph graph;
+
+        [MenuItem("XR/GraphView")]
+        public static XRGraphWindow OpenWithGraph()
+        {
+            var graphWindow = CreateWindow<XRGraphWindow>();
+
+            // When the graph is opened from the window, we don't save the graph to disk
+            graphWindow.graph = ScriptableObject.CreateInstance<BaseGraph>();
+            graphWindow.graph.hideFlags = HideFlags.HideAndDontSave;
+            graphWindow.InitializeGraph(graphWindow.graph);
+
+            graphWindow.Show();
+
+            return graphWindow;
+        }
+
+        protected override void OnDestroy()
+        {
+            graphView?.Dispose();
+            DestroyImmediate(graph);
+        }
+
+        protected override void InitializeWindow(BaseGraph graph)
+        {
+            titleContent = new GUIContent("XR Graph");
+
+            if (graphView == null)
+            {
+                graphView = new XRGraphView(this);
+                //graphView.Add(new CustomToolbarView(graphView));
+            }
+
+            rootView.Add(graphView);
+        }
+    }
+}
